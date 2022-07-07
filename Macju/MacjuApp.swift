bfbe22f9dcf012d8023dadf205cf4d03ce13b0a1
Changsu Lee <729mail2@gmail.com>
2022-07-07 23:10:59 +0900

    Initial Commit

---
+++ Macju/MacjuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacjuApp.swift
+//  Macju
+//
+//  Created by Changsu Lee on 2022/07/07.
+//
+
+import SwiftUI
+
+@main
+struct MacjuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
